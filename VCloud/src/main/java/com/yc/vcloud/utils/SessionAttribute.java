73b882ffe2b15a0bca7fa650946b4cc596c4a013
WildInfoLuo <635809507@qq.com>
2016-09-04 22:14:49 +0800

    简单登录过滤器

---
+++ VCloud/src/main/java/com/yc/vcloud/utils/SessionAttribute.java
@@ -0,0 +1,7 @@
+package com.yc.vcloud.utils;
+
+public class SessionAttribute {
+	public final static String ADMINLOGIN="adminLogin";
+	public final static String USERLOGIN="userLogin";//登录验证
+	public final static String TELRLOGIN="telLogin";//登录验证
+}
